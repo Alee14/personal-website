2bfefbdb7d4a36d20c8396943dde423d54ed548d
Andrew Lee <alee14498@protonmail.com>
2020-04-30 15:23:02 -0400

    Hopefully this works

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'title', name: 'title', content: 'Andrew Lee' },
       { hid: 'description', name: 'description', content: 'Andrew\'s Personal Website' }
     ],
     link: [
