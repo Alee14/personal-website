03097455a0584e46fb4c4c36e5b0bfce7e028be3
Andrew Lee <alee14498@protonmail.com>
2020-08-20 00:02:47 -0400

    Whoops!; Added description to package.json

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,7 @@
 <template>
         <v-container>
-            <h1 v-if="error.statusCode = 404" class="display-3 text-center">404 Error</h1>
-            <h1 v-else>An error occurred</h1>
+            <h1 v-if="error.statusCode = 404" class="display-3 text-center">Whoops! 404 Error</h1>
+            <h1 v-else>Whoops! An error occurred</h1>
             <p class="text-center display-1">Hmm. Seems like this page either didn't exist or something went wrong...</p>
             <p class="text-center display-1"><nuxt-link to="/">Home</nuxt-link></p>
         </v-container>
