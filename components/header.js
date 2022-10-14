6570bf5f1ad7fe9aebab08c78c4111717788948a
Andrew Lee <alee14498@protonmail.com>
2022-10-14 12:28:03 -0400

    Fixed export problems

---
+++ components/header.js
@@ -0,0 +1,13 @@
+import Head from "next/head.js"
+
+const Header = () => {
+    <Head>
+        <meta name="description" content="Andrew's personal website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="icon" href="/favicon.ico" />
+        
+        <title></title>
+    </Head>
+}
+
+export default Header
