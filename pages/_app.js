68044196acf48e42e64ef4aac9ce81830c04a292
Andrew Lee <alee14498@protonmail.com>
2022-10-26 00:55:54 -0400

    Added Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import '../styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Component {...pageProps} />
+        <Analytics />
+      </>
+  )
 }
 
 export default MyApp
