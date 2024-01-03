dd7a71191a9e887624b2c537a00d8f30bab39337
Andrew Lee <alee14498@protonmail.com>
2024-01-02 21:28:31 -0500

    Finished first part of homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [icon()],
+});
