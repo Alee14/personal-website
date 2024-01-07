b34c5413633ff4faffc626142f0cc83175d94747
Andrew Lee <alee14498@protonmail.com>
2024-01-06 23:41:04 -0500

    Add Astro Vercel serverless adapter and update Astro version in package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [icon()],
+  integrations: [icon()],
+  output: "server",
+  adapter: vercel()
 });
