8fd6f1a2739b686092bbb5806666e63f283d6510
Andrew Lee <alee14498@protonmail.com>
2024-01-22 19:56:50 -0500

    Add Vue.JS for Navbar; Transitions; Getting started on projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 
+import vue from '@astrojs/vue';
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  integrations: [icon(), vue()],
   output: "server",
   adapter: vercel()
 });
---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <button class="nav-toggle" aria-label="toggle navigation" @click="toggleNav">
+      <span class='hamburger'></span>
+    </button>
+    <ul class="nav-list">
+      <li><a href="/" class="nav-link" @click="toggleNav">Home</a></li>
+      <li><a href="/projects" class="nav-link" @click="toggleNav">Projects</a></li>
+      <li><a href="/downloads" class="nav-link" @click="toggleNav">Downloads</a></li>
+      <li><a href="/blog" class="nav-link" @click="toggleNav">Blog</a></li>
+      <li><a href="/guestbook" class="nav-link" @click="toggleNav">Guestbook</a></li>
+      <li><a href="/contacts" class="nav-link" @click="toggleNav">Contacts</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  methods: {
+    toggleNav() {
+      document.querySelector('.nav-toggle').classList.toggle('active');
+      document.querySelector('.nav-list').classList.toggle('show');
+    }
+  }
+}
+</script>
+
+<style scoped>
+@import "../styles/Navbar.css";
+</style>
