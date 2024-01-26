ed2c9768a0471d11a0b87a5514dcbf387ebbf8f7
Andrew Lee <alee14498@protonmail.com>
2024-01-25 20:09:56 -0500

    Markdown cb style; Fixed chrome-only issue; Format date on blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,5 +12,19 @@ export default defineConfig({
   adapter: vercel(),
   image: {
     service: passthroughImageService(),
-  }
+  },
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'dracula',
+      // Alternatively, provide multiple themes
+      // https://shikiji.netlify.app/guide/dual-themes#light-dark-dual-themes
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+      // Add custom transformers: https://shikiji.netlify.app/guide/transformers
+      // Find common transformers: https://shikiji.netlify.app/packages/transformers
+      transformers: [],
+    },
+  },
 });
---
+++ src/util.ts
@@ -0,0 +1,8 @@
+// Format date to a string
+function formatDate(date: Date): string {
+    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+
+    return new Date(date).toLocaleDateString(undefined, options);
+}
+
+export { formatDate }
