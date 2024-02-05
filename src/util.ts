dfdf64af769b97d2342cf8b3c6fa25106bfb9acf
Andrew Lee <alee14498@protonmail.com>
2024-02-05 09:52:51 -0500

    Formatted date

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,13 @@
 // Format date to a string
 function formatDate(date: Date): string {
-    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+    const options: Intl.DateTimeFormatOptions = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone
+    };
 
     return new Date(date).toLocaleDateString(undefined, options);
 }
