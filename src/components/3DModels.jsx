4300523e4e166b1ad2adc91f870f10a6c40c8a7c
Andrew Lee <andrew@alee14.me>
2026-07-06 21:41:44 -0400

    fixed 3d models issue

--- src/components/3DModels.jsx
+++ src/components/3DModels.jsx
@@ -1,6 +1,6 @@
 import { Canvas, useLoader } from "@react-three/fiber";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const Model = ({ model, scale }) => {
     const gltf = useLoader(GLTFLoader, model);
